8a38c822f209b1dd1961b6746253911db16b9268
LukaszArndt <lukasz.arndt@googlemail.com>
2016-08-02 12:06:08 +0200

    Ident correction

--- Drawable.h
+++ Drawable.h
@@ -1,8 +1,8 @@
 class IRenderContext
 {
     void Update(double time) = 0;	
-	bool PreRender() = 0;	
+    bool PreRender() = 0;	
     void Render() = 0;	
-	void PostRender() = 0;	
+    void PostRender() = 0;	
 }
 
